af0042b4c0538f517bca0f87a1a7b2c06be74a63
szymon <szymon.kaube@gmail.com>
2026-01-28 13:26:31 +0100

    set default zsh editor to neovim

--- .zshrc.d/exports.zsh
+++ .zshrc.d/exports.zsh
@@ -2,3 +2,7 @@ export PATH="$HOME/.local/bin:$PATH"
 
 # not use PEAR as php package manager (we use composer)
 export PHP_CONFIGURE_OPTIONS="--with-openssl --with-curl --without-pear"
+
+# default editor
+export EDITOR=nvim
+export VISUAL=$EDITOR
